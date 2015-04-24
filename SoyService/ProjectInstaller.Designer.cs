bee0c92c2225000b5c64360b4719f0462dc68e60
HansJon <HJIvars@hotmail.com>
2015-04-24 11:50:39 +0000

    SoyService Email Schedule Program
    Soy Service Process to Handle Email Scheduling

---
+++ SoyService/ProjectInstaller.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace SoyService
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.SoyServiceInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalService;
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+            // 
+            // SoyServiceInstaller
+            // 
+            this.SoyServiceInstaller.Description = "Soy Service is a process used to mail work logs to Tokyo Sushi";
+            this.SoyServiceInstaller.DisplayName = "Soy Service";
+            this.SoyServiceInstaller.ServiceName = "SoyService";
+            this.SoyServiceInstaller.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller1,
+            this.SoyServiceInstaller});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller SoyServiceInstaller;
+    }
+}
---
+++ SoyService/SoyService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+namespace SoyService
+{
+    public partial class SoyService : ServiceBase
+    {
+        public SoyService()
+        {
+            InitializeComponent();
+        }
+
+        public void OnDebug()
+        {
+            OnStart(null);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            MessageBox.Show("Soy Service Started");
+
+            try
+            {
+                System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "OnStart.txt");
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Could not create file");
+            }
+        }
+
+        protected override void OnStop()
+        {
+            MessageBox.Show("Soy Service Stopped");
+
+            try
+            {
+                System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "OnStop.txt");
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Could not create file");
+            }
+        }
+    }
+}
